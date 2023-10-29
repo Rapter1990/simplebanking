035d2a4a6c0de2d3fb01d4c397bc430f62403771
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-10-29 19:54:57 +0300

    Task 8 : Implement the process of exception handling with ErrorResponse and GlobalExceptionHandler

---
+++ src/main/java/com/eteration/simplebanking/exception/response/ErrorResponse.java
@@ -0,0 +1,31 @@
+package com.eteration.simplebanking.exception.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+    private HttpStatus status;
+
+    private Integer statusCode;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<String> errorDetails;
+
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+}
