dbd65a79f052796e05298303213974aea47acea1
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-10-29 19:16:16 +0300

    Task 4 : Delete irrelevant classes

--- src/main/java/com/eteration/simplebanking/model/InsufficientBalanceException.java
+++
@@ -1,6 +0,0 @@
-package com.eteration.simplebanking.model;
-
-
-// This class is a place holder you can change the complete implementation
-public class InsufficientBalanceException extends Exception {
-}
