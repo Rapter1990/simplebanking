8c66a4fb4e415179be4a864d2a5d2cd7bf2e3149
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-10-30 00:21:43 +0300

    Task 20 : Add BaseControllerTest and BaseServiceTest

---
+++ src/test/java/com/eteration/simplebanking/base/BaseControllerTest.java
@@ -0,0 +1,12 @@
+package com.eteration.simplebanking.base;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(value = "test")
+public abstract class BaseControllerTest {
+
+}
---
+++ src/test/java/com/eteration/simplebanking/base/BaseServiceTest.java
@@ -0,0 +1,9 @@
+package com.eteration.simplebanking.base;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class BaseServiceTest {
+
+}
