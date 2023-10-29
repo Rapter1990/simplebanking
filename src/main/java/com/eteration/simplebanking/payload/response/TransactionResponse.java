9282f94af5f0be8cd4d7c5f9ae5a16c2180e514b
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-10-29 20:27:27 +0300

    Task 11 : Define TransactionResponse

---
+++ src/main/java/com/eteration/simplebanking/payload/response/TransactionResponse.java
@@ -0,0 +1,15 @@
+package com.eteration.simplebanking.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionResponse {
+    private String status;
+    private String approvalCode;
+}
