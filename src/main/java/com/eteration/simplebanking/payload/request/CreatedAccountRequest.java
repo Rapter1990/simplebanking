5c32f2564c2f0c79de77e6c7976e99f948676e89
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-10-29 23:41:00 +0300

    Task 17 : Add CreatedAccountRequest and CreatedAccountResponse

---
+++ src/main/java/com/eteration/simplebanking/payload/request/CreatedAccountRequest.java
@@ -0,0 +1,14 @@
+package com.eteration.simplebanking.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreatedAccountRequest {
+    private String owner;
+}
