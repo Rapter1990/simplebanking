4df5e0938be245f70f8fa38f167aeeecb8021fed
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-10-30 09:38:07 +0300

    Task 25 : Define AccountDetailInfo

---
+++ src/main/java/com/eteration/simplebanking/payload/response/AccountDetailInfo.java
@@ -0,0 +1,20 @@
+package com.eteration.simplebanking.payload.response;
+
+import com.eteration.simplebanking.dto.TransactionDTO;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AccountDetailInfo {
+    private String accountNumber;
+    private String owner;
+    private Double balance;
+    private LocalDateTime createdDateTime;
+    List<TransactionDTO> transactionDTOs;
+}
