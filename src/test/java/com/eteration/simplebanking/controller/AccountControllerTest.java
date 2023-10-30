0f83f050d94a33aae7ffc0eff55c4a769b3d7f39
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-10-30 09:39:45 +0300

    Task 27 : Implement the test process of getting account info by account number in AccountController and AccountService

--- src/test/java/com/eteration/simplebanking/controller/AccountControllerTest.java
+++ src/test/java/com/eteration/simplebanking/controller/AccountControllerTest.java
@@ -1,14 +1,28 @@
 package com.eteration.simplebanking.controller;
 
 import com.eteration.simplebanking.base.BaseControllerTest;
+import com.eteration.simplebanking.dto.AccountDTO;
+import com.eteration.simplebanking.mapper.AccountMapper;
+import com.eteration.simplebanking.model.Account;
 import com.eteration.simplebanking.payload.request.CreatedAccountRequest;
+import com.eteration.simplebanking.payload.response.AccountDetailInfo;
+import com.eteration.simplebanking.payload.response.CreatedAccountResponse;
+import com.eteration.simplebanking.services.AccountService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class AccountControllerTest extends BaseControllerTest {
@@ -19,6 +33,12 @@ class AccountControllerTest extends BaseControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private AccountService accountService;
+
+    @MockBean
+    private AccountMapper accountMapper;
+
     @Test
     public void givenCreatedAccountRequest_whenCreateAccount_ReturnSavedAccount() throws Exception {
 
@@ -27,16 +47,74 @@ class AccountControllerTest extends BaseControllerTest {
                 .owner("John Doe")
                 .build();
 
-        // Perform the POST request
+
+        AccountDTO accountDTO = AccountDTO.builder()
+                .owner(request.getOwner())
+                .accountNumber("123456")
+                .createdDateTime(LocalDateTime.now())
+                .balance(0.0)
+                .transactionDTOs(new ArrayList<>())
+                .build();
+
+        CreatedAccountResponse createdAccountResponse = CreatedAccountResponse.builder()
+                .owner(accountDTO.getOwner())
+                .accountNumber(accountDTO.getAccountNumber())
+                .balance(accountDTO.getBalance())
+                .createdDateTime(accountDTO.getCreatedDateTime())
+                .transactionDTOs(accountDTO.getTransactionDTOs())
+                .build();
+
+        // when
+        when(accountService.create(request)).thenReturn(accountDTO);
+        when(accountMapper.toAccountResponse(accountDTO)).thenReturn(createdAccountResponse);
+
+        // then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/account/create")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.owner").value("John Doe"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").value("123456"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.balance").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.transactionDTOs").isArray());
+
+    }
+
+    @Test
+    public void givenAccountByAccountNumber_WhenAccountExist_ThenAccountDetailInfo() throws Exception {
+
+        // Given
+        String accountNumber = "123456789";
+
+        AccountDTO accountDTO = AccountDTO.builder()
+                .owner("John Doe")
+                .accountNumber(accountNumber)
+                .createdDateTime(LocalDateTime.now())
+                .balance(0.0)
+                .transactionDTOs(new ArrayList<>())
+                .build();
+
+        AccountDetailInfo accountDetailInfo = AccountDetailInfo.builder()
+                .owner(accountDTO.getOwner())
+                .accountNumber(accountDTO.getAccountNumber())
+                .balance(accountDTO.getBalance())
+                .createdDateTime(accountDTO.getCreatedDateTime())
+                .transactionDTOs(accountDTO.getTransactionDTOs())
+                .build();
+
+        // when
+        when(accountService.getAccountByAccountNumber(accountNumber)).thenReturn(accountDTO);
+        when(accountMapper.toAccountDetailInfo(accountDTO)).thenReturn(accountDetailInfo);
+
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/account/account-number/{accountNumber}", accountNumber))
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.owner").value("John Doe"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").isString())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.balance").isNumber())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.transactionDTOs").isArray());
-
     }
+
 }
--- src/test/java/com/eteration/simplebanking/services/AccountServiceTest.java
+++ src/test/java/com/eteration/simplebanking/services/AccountServiceTest.java
@@ -3,6 +3,7 @@ package com.eteration.simplebanking.services;
 import com.eteration.simplebanking.base.BaseServiceTest;
 import com.eteration.simplebanking.dto.AccountDTO;
 import com.eteration.simplebanking.dto.TransactionDTO;
+import com.eteration.simplebanking.exception.AccountNotFoundException;
 import com.eteration.simplebanking.mapper.TransactionMapper;
 import com.eteration.simplebanking.model.Account;
 import com.eteration.simplebanking.payload.request.CreatedAccountRequest;
@@ -15,6 +16,7 @@ import org.mockito.Mock;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -70,4 +72,49 @@ class AccountServiceTest extends BaseServiceTest {
         verify(transactionMapper, times(2)).toTransactionDTOList(any(ArrayList.class));
 
     }
+
+    @Test
+    public void givenAccountByAccountNumber_WhenAccountExist_ThenAccountDetailInfo() {
+        String accountNumber = "123456789";
+
+        Account account = Account.builder()
+                .accountNumber("123-456")
+                .owner("John Doe")
+                .transactions(new HashSet<>())
+                .build();
+
+        List<TransactionDTO> transactions = account.getTransactions().stream()
+                .map(transactionMapper::toTransactionDTO)
+                .collect(Collectors.toList());
+
+        when(accountRepository.findByAccountNumber(accountNumber)).thenReturn(Optional.of(account));
+
+        AccountDTO accountDTO = accountService.getAccountByAccountNumber(accountNumber);
+
+        assertEquals(account.getOwner(), accountDTO.getOwner());
+        assertEquals(account.getAccountNumber(), accountDTO.getAccountNumber());
+        assertEquals(account.getBalance(), accountDTO.getBalance());
+        assertEquals(transactions, accountDTO.getTransactionDTOs());
+
+        verify(accountRepository,times(1)).findByAccountNumber(accountNumber);
+
+    }
+
+    @Test
+    public void givenAccountByAccountNumber_WhenAccountDoesNotExist_ThenThrowAccountNotFoundException() {
+
+        // Given
+        String accountNumber = "123456789";
+
+        // Mock the behavior of the repository to return an empty optional, simulating an account not found
+        when(accountRepository.findByAccountNumber(accountNumber)).thenReturn(Optional.empty());
+
+        // When and Then
+        AccountNotFoundException exception = assertThrows(AccountNotFoundException.class, () -> {
+            accountService.getAccountByAccountNumber(accountNumber);
+        });
+
+        assertEquals("Account Not Found : " + accountNumber, exception.getMessage());
+    }
+
 }
