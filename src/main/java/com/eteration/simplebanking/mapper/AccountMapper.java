f95d7d58c63553fcd815527332bac2f101855946
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-11-03 21:21:44 +0300

    Task 68 : Add Java Doc for AccountMapper, TransactionMapper

--- src/main/java/com/eteration/simplebanking/mapper/AccountMapper.java
+++ src/main/java/com/eteration/simplebanking/mapper/AccountMapper.java
@@ -5,11 +5,26 @@ import com.eteration.simplebanking.payload.response.AccountDetailInfo;
 import com.eteration.simplebanking.payload.response.CreatedAccountResponse;
 import org.mapstruct.Mapper;
 
+/**
+ * Mapper interface for mapping between AccountDTO and response objects in the Simple Banking App.
+ */
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
 
+    /**
+     * Maps an AccountDTO to a CreatedAccountResponse object.
+     *
+     * @param accountDTO The source AccountDTO to be mapped.
+     * @return A CreatedAccountResponse object.
+     */
     CreatedAccountResponse toAccountResponse(AccountDTO accountDTO);
 
+    /**
+     * Maps an AccountDTO to an AccountDetailInfo object.
+     *
+     * @param accountDTO The source AccountDTO to be mapped.
+     * @return An AccountDetailInfo object.
+     */
     AccountDetailInfo toAccountDetailInfo(AccountDTO accountDTO);
 
 }
--- src/main/java/com/eteration/simplebanking/mapper/TransactionMapper.java
+++ src/main/java/com/eteration/simplebanking/mapper/TransactionMapper.java
@@ -7,11 +7,26 @@ import org.mapstruct.Mapping;
 
 import java.util.List;
 
+/**
+ * Mapper interface for mapping between Transaction entities and DTOs in the Simple Banking App.
+ */
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
 
+    /**
+     * Maps a Transaction entity to a TransactionDTO.
+     *
+     * @param transaction The source Transaction entity to be mapped.
+     * @return A TransactionDTO object.
+     */
     @Mapping(target = "transactionType", source = "transactionType")
     TransactionDTO toTransactionDTO(Transaction transaction);
 
+    /**
+     * Maps a list of Transaction entities to a list of TransactionDTOs.
+     *
+     * @param transactions The source list of Transaction entities to be mapped.
+     * @return A list of TransactionDTO objects.
+     */
     List<TransactionDTO> toTransactionDTOList(List<Transaction> transactions);
 }
